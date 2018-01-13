6a7549aee984e577ffbea2f71de79d52ec8d0e16
Veronica Miller <vmcmiller@gmail.com>
2018-01-13 18:32:52 +0000

    updated book and user models

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ActiveReord::Base
+  has_many :books
+  has_secure_password
+
+  def self.current_user(session)
+    User.find(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    !!session[:user_id]
+  end
+
+  def slug
+    self.username.split(" ").collect {|word| word.downcase}.join("-")
+  end
+
+  def self.find_by_slug(slug)
+    self.where("lower(username) = ?", slug.split("-").join(" ").downcase).first
+  end
+
+end
